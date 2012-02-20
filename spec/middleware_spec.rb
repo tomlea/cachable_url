828435eb433ac1fa1643c96491856b3035f1f712
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-20 22:50:36 +0000

    Typos make me sad

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
-describe CachableUrl::Midleware do
+describe CachableUrl::Middleware do
   include Rack::Test::Methods
 
   let(:requests_received){ [] }
 
   def app
     logger_app = lambda{|env| requests_received << env; [200, {"Content-Type" => 'text/plain'}, ["Hello world"]] }
-    CachableUrl::Midleware.new(logger_app)
+    CachableUrl::Middleware.new(logger_app)
   end
 
   it "should pass through requests with query strings" do
@@ -31,12 +31,12 @@ describe CachableUrl::Midleware do
     get "/fooooooo?foo=bar"
     
     requests_received.size.should == 2
-    a,b = clense_responses(*requests_received.last(2))
+    a,b = cleanse_responses(*requests_received.last(2))
     a.should == b
   end
 
   protected
-  def clense_responses(*args)
+  def cleanse_responses(*args)
     args.map{|r|
       r.delete_if{|key, value|
         ["rack.input", "rack.errors"].include? key
